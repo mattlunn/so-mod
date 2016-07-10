efde9a73f3da34534cd1290410891acab6123350
Matt Lunn <matt@mattlunn.me.uk>
2016-07-10 19:31:48 +0100

    making remote urls' protocol relative

--- moderator.user.js
+++ moderator.user.js
@@ -246,7 +246,7 @@
 							}
 
 							// http://stackoverflow.com/a/2117523/444991
-							remote.val('http://sandbox.mattlunn.me.uk/userscript/?id=' + ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+							remote.val(window.location.protocol + '//sandbox.mattlunn.me.uk/userscript/?id=' + ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 								var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 								return v.toString(16);
 							})));
