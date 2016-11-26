6e73993aff4a8cab9b08c8ab34db6ac6eda53dfa
Matt <matt@mattlunn.me.uk>
2016-11-26 12:07:45 +0000

    Updating list of $matches

--- remote.php
+++ remote.php
@@ -1,12 +1,20 @@
 <?php
 
-$matches = array('/^http(s)?:\/\/stackoverflow\.com/', '/^http(s)?:\/\/(meta\.)?([a-z]+)\.stackexchange\.com');
+$matches = array(
+        '/^http(s)?:\/\/(meta\.)?([a-z]+)\.stackexchange\.com/',
+        '/^http(s)?:\/\/(meta\.)?mathoverflow\.net/',
+        '/^http(s)?:\/\/(meta\.)?serverfault\.com/',
+        '/^http(s)?:\/\/(meta\.)?stackapps\.com/',
+        '/^http(s)?:\/\/(meta\.)?stackexchange\.com/',
+        '/^http(s)?:\/\/(meta\.)?stackoverflow\.com/',
+        '/^http(s)?:\/\/(meta\.)?superuser\.com/',
+);
 
 foreach ($matches as $match) {
-	if (preg_match($match, $_SERVER['HTTP_ORIGIN'])) {
-		header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-		break;
-	}
+    if (preg_match($match, $_SERVER['HTTP_ORIGIN'])) {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        break;
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
