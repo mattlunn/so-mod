c21a66bd876b5a42893337b0a87517767858ff45
Matt <matt@mattlunn.me.uk>
2016-11-30 11:16:57 +0000

    bump version

--- moderator.user.js
+++ moderator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Helpful Moderator Userscripts
 // @namespace    https://github.com/mattlunn/so-mod
-// @version      1.12
+// @version      1.13
 // @author       Matt
 // @match       *://*.askubuntu.com/*
 // @match       *://*.mathoverflow.net/*
